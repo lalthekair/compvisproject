d59b3b763176d809373cd6e45e5febf455bf137c
Leena Althekair <107867477+lalthekair@users.noreply.github.com>
2024-05-16 18:47:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import subprocess
 
 def run_yolov5_detection(source_path):
-    command = f"python yolov5/detect.py --weights yolov5s.pt --source {source_path} --conf 0.5 --classes 0 2 5 7 3 4"
+    command = f"python yolov5/detect.py --weights yolov5s.pt --source {source_path} --conf 0.5 --classes 2 5 7 3 6"
     subprocess.run(command, shell=True)
 
 if __name__ == "__main__":
-    source_path = "vid1.mp4"
+    source_path = "img6.jpg"
     run_yolov5_detection(source_path)
