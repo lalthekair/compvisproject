a4f5fc84f51e1c1272075a5920804276c3fa9319
Leena Althekair <107867477+lalthekair@users.noreply.github.com>
2024-05-18 00:42:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ def run_yolov5_detection(source_path):
 if __name__ == "__main__":
     source_path = "img6.jpg"
     run_yolov5_detection(source_path)
+
+"""
+Make sure that the image you want to run this on and the .py file are in the same directory.
+Create a virtual environment in the directory, then install yolov5 model as per: https://github.com/ultralytics/yolov5/
+
+Run these commands in the terminal one by one (after activating the virtual env)
+git clone https://github.com/ultralytics/yolov5  # clone repository
+cd yolov5
+pip install -r requirements.txt  # install yolov5
+
+Results of the runs are stored in yolov5\runs\detect\exp (number indicates run #)
+"""
